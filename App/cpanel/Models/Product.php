1451f487ee9b5b71380116dedcda5ee6eb84eb0a
TeRmiT <termit99@bk.ru>
2017-03-03 23:56:38 +0300

    update 0.0.5.5 - add models Product

---
+++ App/cpanel/Models/Product.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\cpanel\Models;
+
+
+use App\cpanel\Model;
+
+class Product
+    extends Model
+{
+    const TABLE = 'products';
+
+    public $title;
+    public $lead;
+    public $description;
+    public $category_id;
+
+    public function __get($k){
+        switch ($k){
+            case 'category':
+                return User::findById($this->category_id);
+                break;
+            default:
+                return null;
+        }
+    }
+    public function __isset($k)
+    {
+        switch ($k){
+            case 'category':
+                return !empty($this->category_id);
+                break;
+            default:
+                return false;
+        }
+    }
+
+    public function setTitle($str)
+    {
+        $this->title = trim($str);
+    }
+
+    public function fill($data = []){
+        $e = new MultiException();
+        if (true){
+            $e[] = new \Exception('Заголовок неверный');
+        }
+        if (true){
+            $e[] = new \Exception('Текст неверный');
+        }
+        throw $e;
+    }
+
+    public function preInsert($data){
+        foreach ($data as $res => $val){
+            $this->$res = $val;
+        }
+        return [];
+    }
+}
